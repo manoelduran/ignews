938f55abcd238b70e5a65bf4046364ed5097735a
Manoel Duran <manoel.duran@hotmail.com>
2021-07-28 14:06:48 -0300

    finalizando deploy

--- src/pages/posts/preview/[slug].tsx
+++ src/pages/posts/preview/[slug].tsx
@@ -52,7 +52,7 @@ export default function PostPreview({ post }: PostPreviewProps) {
 export const getStaticPaths: GetStaticPaths = async () => {
   return {
     paths: [],
-    fallback: true
+    fallback: 'blocking'
   }
 }
 
